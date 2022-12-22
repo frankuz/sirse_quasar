7503ea9492b9923ba684b4dbcb703036efa0474f
frankuz <franklinuribe@gmail.com>
2022-12-22 17:32:27 -0500

    preparando runners

---
+++ src/components/DialogWaiting.vue
@@ -0,0 +1,20 @@
+<template>
+  <q-dialog v-model="app.waitingMsgIsVisible">
+    <q-card style="max-width: 500px">
+      <q-card-section class="q-pt-none text-center">
+        <q-spinner-ios
+          color="primary"
+          size="5em"
+          :thickness="3"
+          class="q-my-lg"
+        />
+        <div>{{app.waitingMessage}}</div>
+      </q-card-section>
+    </q-card>
+  </q-dialog>
+</template>
+
+<script setup>
+import { useAppStore } from 'stores/app'
+const app = useAppStore()
+</script>
