f7bf9c44ad937b5dc7fe31251f49c1f78916da8a
frankuz <franklinuribe@gmail.com>
2022-12-29 07:14:52 -0500

    mejorado el Logger

--- src/components/LoggerStore.vue
+++ src/components/LoggerStore.vue
@@ -4,21 +4,29 @@
       <td><pre>{{ app.iniciativaActiva }}</pre></td>
       <td><pre>{{ app.iniciativaEditable }}</pre></td>
     </tr>
-    <tr>
-      <td colspan="2"><pre>{{ app.beneficiariosActivos }}</pre></td>
-    </tr>
-    <tr>
-      <td colspan="2"><pre>{{ app.proyectosdcActivos }}</pre></td>
-    </tr>
-    <tr>
-      <td colspan="2"><pre>{{ app.aliadosActivos }}</pre></td>
-    </tr>
   </table>
+    <div v-for="reporte in reportes" :key="reporte">
+      <div class="text-h6">{{ reporte }}</div>
+      <table v-for="(item, i) in app[reporte]" :key="i">
+        <tr>
+          <td colspan="2"><pre>esNuevo: {{ item.esNuevo }}</pre></td>
+        </tr>
+        <tr>
+          <td><pre>{{ item.actual }}</pre></td>
+          <td><pre>{{ item.editable }}</pre></td>
+        </tr>
+      </table>
+    </div>
 </template>
 
 <script setup>
 // import { useQuasar } from 'quasar'
+import { ref } from 'vue'
 import { useAppStore } from 'stores/app'
-
 const app = useAppStore()
+const reportes = ref([
+  'beneficiariosActivos',
+  'proyectosActivos',
+  'aliadosActivos'
+])
 </script>
